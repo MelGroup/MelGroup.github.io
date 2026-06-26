75b24e3d952e463681879b42cc4229a9ff1fbc9a
糖豆 <3177437330@qq.com>
2026-06-27 04:51:43 +0800

    Add files via upload

--- MoFiger/script.js
+++ MoFiger/script.js
@@ -9,10 +9,10 @@
             contact_email_label: '联系邮箱：'
         },
         ja: {
-            founded: 'MoFiger2026.6.27',
-            hero_line2: 'MoFiger',
-            hero_desc: 'MoFiger——メロルセリソースリスト、MSUTファイルライブラリ、DSLツールを統合したAPP!',
-            download_btn: 'MoFigerをダウンロード',
+            founded: 'マカロン2026.6.27',
+            hero_line2: 'マカロン',
+            hero_desc: 'マカロン——メロルセリソースリスト、MSUTファイルライブラリ、DSLツールを統合したAPP!',
+            download_btn: 'マカロンをダウンロード',
             footer_partner_prefix: 'パートナー：',
             contact_email_label: '連絡先：'
         }
